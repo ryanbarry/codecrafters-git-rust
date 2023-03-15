7b09de320d8915a5c8c505fca6f0d07a8d7314a6
Ryan Barry <ryan@nuclearice.com>
2023-03-14 21:26:42 -0700

    start parsing object header

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
 use std::path::PathBuf;
 use std::process::ExitCode;
+use std::str::FromStr;
 
 use clap::{Parser, Subcommand};
 use flate2::read::ZlibDecoder;
@@ -18,13 +20,13 @@ enum Commands {
         #[arg(short, help = "pretty-print <object> content")]
         pretty_print: bool,
         #[arg()]
-        blob_sha: String,
+        obj_sha: String,
     },
 }
 
 fn main() -> ExitCode {
     let ret_not_impl: ExitCode = ExitCode::from(1);
-    let ret_invalid_blobsha: ExitCode = ExitCode::from(128);
+    let ret_invalid_objsha: ExitCode = ExitCode::from(128);
 
     let cli = Cli::parse();
 
@@ -39,36 +41,71 @@ fn main() -> ExitCode {
         }
         Commands::CatFile {
             pretty_print,
-            blob_sha,
+            obj_sha,
         } => {
             if !pretty_print {
                 println!("cat-file without pretty-print not implemented");
                 return ret_not_impl;
             }
-            if is_plausibly_blob_sha(blob_sha) {
-                let p = blob_path_from_sha(blob_sha);
+            if is_plausibly_obj_sha(obj_sha) {
+                let p = obj_path_from_sha(obj_sha);
                 if let Ok(blobfile) = File::open(p) {
-                    let mut z = ZlibDecoder::new(&blobfile);
-                    std::io::copy(&mut z, &mut std::io::stdout()).expect("failed to read blob");
+                    let (objtype, objsz, mut reader) = object_decoder(blobfile);
+                    std::io::copy(&mut reader, &mut std::io::stdout());
                     return ExitCode::SUCCESS;
                 } else {
-                    return ret_invalid_blobsha;
+                    return ret_invalid_objsha;
                 }
             } else {
-                println!("fatal: Not a valid object name {}", blob_sha);
-                return ret_invalid_blobsha;
+                println!("fatal: Not a valid object name {}", obj_sha);
+                return ret_invalid_objsha;
             }
         }
     }
 }
 
-fn is_plausibly_blob_sha(maybe_blob_sha: &str) -> bool {
-    maybe_blob_sha.len() == 40 && maybe_blob_sha.chars().all(|c| c.is_ascii_hexdigit())
+fn is_plausibly_obj_sha(maybe_obj_sha: &str) -> bool {
+    maybe_obj_sha.len() == 40 && maybe_obj_sha.chars().all(|c| c.is_ascii_hexdigit())
 }
 
-fn blob_path_from_sha(blob_sha: &str) -> PathBuf {
-    let (obj_dirname, obj_filename) = blob_sha.split_at(2);
+fn obj_path_from_sha(obj_sha: &str) -> PathBuf {
+    let (obj_dirname, obj_filename) = obj_sha.split_at(2);
     [".git", "objects", obj_dirname, obj_filename]
         .iter()
         .collect()
+}
+
+enum ObjType {
+    BLOB,
+    COMMIT,
+    Tree,
+}
+
+fn object_decoder(object: File) -> (ObjType, usize, BufReader<ZlibDecoder<File>>) {
+    let mut z = ZlibDecoder::new(object);
+
+    let mut magic = [0u8; 5];
+    if let Err(e) = z.read_exact(&mut magic) {
+        panic!("{}", e); // TODO
+    }
+    let mut brzdf = BufReader::new(z);
+    let mut objsz = vec![];
+    match &magic {
+        b"blob " => {
+            brzdf.read_until(0u8, &mut objsz);
+            objsz.pop(); // remove terminating null byte before parsing
+            let objsz = usize::from_str(&String::from_utf8(objsz).unwrap()).expect("blob header concludes with object len");
+
+            return (ObjType::BLOB, objsz, brzdf);
+        }
+        b"tree " => {
+            return (ObjType::Tree, 0, brzdf);
+        }
+        b"commi" => {
+            return (ObjType::COMMIT, 0, brzdf);
+        }
+        _ => {
+            return (ObjType::BLOB, 0, brzdf);
+        }
+    }
 }
