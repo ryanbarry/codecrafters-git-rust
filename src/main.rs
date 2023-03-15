b9243f76a2e9db9b220e5d2294a907cc2472d151
Ryan Barry <ryan@nuclearice.com>
2023-03-14 20:26:18 -0700

    continue fleshing out structure & simple fxnality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::path::PathBuf;
+use std::process::ExitCode;
 
 use clap::{Parser, Subcommand};
 
@@ -14,10 +16,15 @@ enum Commands {
     CatFile {
         #[arg(short, help = "pretty-print <object> content")]
         pretty_print: bool,
+        #[arg()]
+        blob_sha: String,
     },
 }
 
-fn main() {
+fn main() -> ExitCode {
+    let ret_not_impl: ExitCode = ExitCode::from(1);
+    let ret_invalid_blobsha: ExitCode = ExitCode::from(128);
+
     let cli = Cli::parse();
 
     match &cli.command {
@@ -26,8 +33,40 @@ fn main() {
             fs::create_dir(".git/objects").unwrap();
             fs::create_dir(".git/refs").unwrap();
             fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-            println!("Initialized git directory")
+            println!("Initialized git directory");
+            return ExitCode::SUCCESS;
         }
-        _ => todo!(),
+        Commands::CatFile {
+            pretty_print,
+            blob_sha,
+        } => {
+            if !pretty_print {
+                println!("cat-file without pretty-print not implemented");
+                return ret_not_impl;
+            }
+            if is_plausibly_blob_sha(blob_sha) {
+                let p = blob_path_from_sha(blob_sha);
+                println!("path to that blob: {:?}", p);
+                if p.exists() {
+                    println!("and it exists!");
+                    return ExitCode::SUCCESS;
+                } else {
+                    println!("and it does not exist");
+                    return ret_invalid_blobsha;
+                }
+            } else {
+                println!("invalid blob_sha given: {}", blob_sha);
+                return ret_invalid_blobsha;
+            }
+        }
     }
+}
+
+fn is_plausibly_blob_sha(maybe_blob_sha: &str) -> bool {
+    maybe_blob_sha.len() == 40 && maybe_blob_sha.chars().all(|c| c.is_ascii_hexdigit())
+}
+
+fn blob_path_from_sha(blob_sha: &str) -> PathBuf {
+    let (obj_dirname, obj_filename) = blob_sha.split_at(2);
+    [".git", "objects", obj_dirname, obj_filename].iter().collect()
 }
