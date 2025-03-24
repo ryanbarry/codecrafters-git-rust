9f717b9851d7892ed563ca170bc3319660865ad9
Ryan Barry <ryan@nuclearice.com>
2025-03-23 19:11:41 -0700

    update devenv, rust version, etc.

--- flake.nix
+++ flake.nix
@@ -1,56 +1,65 @@
 {
+  description = "A Nix-flake-based Rust development environment";
+
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-    nixpkgs-mozilla = {
-      url = "github:mozilla/nixpkgs-mozilla";
-      flake = false;
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.715729";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, utils, naersk, nixpkgs-mozilla }:
-    utils.lib.eachDefaultSystem (system:
-      let
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs {
           inherit system;
-
-          overlays = [
-            (import nixpkgs-mozilla)
-          ];
+          overlays = [ rust-overlay.overlays.default self.overlays.default ];
         };
+      });
+    in
+    {
+      overlays.default = final: prev: {
+        rustToolchain =
+          let
+            rust = prev.rust-bin;
+          in
+          if builtins.pathExists ./rust-toolchain.toml then
+            rust.fromRustupToolchainFile ./rust-toolchain.toml
+          else if builtins.pathExists ./rust-toolchain then
+            rust.fromRustupToolchainFile ./rust-toolchain
+          else
+            rust.stable.latest.default.override {
+              extensions = [ "rust-src" "rustfmt" ];
+            };
+      };
 
-        toolchain = (pkgs.rustChannelOf {
-          rustToolchain = ./rust-toolchain;
-          sha256 = "sha256-AoqjoLifz8XrZWP7piauFfWCvhzPMLKxfv57h6Ng1oM=";
-        }).rust.override (old: {
-          extensions = [ "rust-src" "rust-analysis" ];
-        });
-
-        naersk-lib = pkgs.callPackage naersk {
-          cargo = toolchain;
-          rustc = toolchain;
-        };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-          src = ./.;
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = with pkgs; [ openssl ];
-        };
+          buildInputs = with pkgs; [
+            openssl
+            rustToolchain
+            rustPackages.clippy
+          ];
+          packages = with pkgs; [
+            rustToolchain
+            lldb
+            gdb
+          ];
 
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
-
-        devShell = with pkgs; mkShell {
-          nativeBuildInputs = [ pkg-config ];
-          buildInputs = [ openssl toolchain rustPackages.clippy ];
-          RUST_LOG = "trace";
           shellHook = ''
           alias mygit="! [[ "\$\(pwd\)" == \"/tmp/testing\" ]] && echo \"unsafe! not in /tmp/testing\" || $PWD/your_git.sh"
           alias rsdoc="firefox --new-tab $(nix-store -q $(which rustc))/share/doc/rust/html/std/index.html"
           '';
+
+          env = {
+            # Required by rust-analyzer
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            RUST_LOG = "trace";
+          };
         };
       });
+    };
 }
